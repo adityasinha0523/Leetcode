ddd8aafcb8afed347ae4b0581ba42334f0715144
Aditya Sinha <adityasinhafb@gmail.com>
2022-08-15 08:26:15 -0700

    Adding 2006

---
+++ problem2006.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.Scanner;
+import java.util.List;
+
+class problem2006{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+ 
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+ 
+            // the code is not running on an online judge
+ 
+            PrintStream ps
+                = new PrintStream(new File("output.txt"));
+            InputStream is
+                = new FileInputStream("input.txt");
+ 
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //For reading integer from input file
+        //int a=scan.nextInt();
+        //String temp1="leetcodeisacommunityforcoders";
+        //int temp2=4;
+        /*int [] input=new int[4];
+        for (int i = 0; i < input.length; i++) {
+            input[i]=scan.nextInt();
+        }*/
+
+        
+        //scan.nextLine();
+        //To take array as input
+        int[] num=new int[5];
+        for (int i = 0; i < num.length; i++) {
+            num[i]=scan.nextInt();
+        }
+        int k=scan.nextInt();
+        int sol=countKDifference(num,k);
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        System.out.println(sol);
+        scan.close();
+    }  
+
+    public static int countKDifference(int[] nums, int k) {
+        int solution=0;
+        for (int i = 0; i < nums.length; i++) {
+            int temp=nums[i];
+            for (int j = i+1; j < nums.length; j++) {
+                if(Math.abs(temp-nums[j])==k){
+                    solution++;
+                }
+            }
+        }
+        return solution;
+    }
+}  
