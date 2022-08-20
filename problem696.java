97ab31ffe1d072d7884f851d87e435af0904d067
Aditya Sinha <adityasinhafb@gmail.com>
2022-08-20 11:19:07 -0700

    problem 696

---
+++ problem696.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.Scanner;
+import java.util.List;
+import java.util.*;
+import java.lang.*;
+
+class problem696{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+ 
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+ 
+            // the code is not running on an online judge
+ 
+            PrintStream ps
+                = new PrintStream(new File("output.txt"));
+            InputStream is
+                = new FileInputStream("input.txt");
+ 
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //For reading integer from input file
+        //int a=scan.nextInt();
+        //String temp1="leetcodeisacommunityforcoders";
+        //int temp2=4;
+        /*int [] input=new int[4];
+        for (int i = 0; i < input.length; i++) {
+            input[i]=scan.nextInt();
+        }*/
+
+        
+        //scan.nextLine();
+        //To take array as input
+        String s =scan.nextLine();
+        int sol=countBinarySubstrings(s);
+        
+        System.out.println(sol);
+        scan.close();
+    }  
+
+    /*public static int countBinarySubstrings(String s) {
+        int cur = 1, pre = 0, res = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) cur++;
+            else {
+                res += Math.min(cur, pre);
+                pre = cur;
+                cur = 1;
+            }
+        }
+        return res + Math.min(cur, pre);
+    }*/
+
+    public static int countBinarySubstrings(String s) {
+        int res=0;
+        int cur=1;
+        int prev=0;
+        for (int i = 1; i < s.length(); i++) {
+            if(s.charAt(i)==s.charAt(i-1)){
+                cur++;
+            }else{
+                res+=Math.min(cur, prev);
+                prev=cur;
+                cur=1;
+            }
+        }
+        return res+Math.min(cur,prev);
+    }
+}  
