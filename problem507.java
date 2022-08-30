76143e7797bb8b62009ebe1fc298875a53151e26
Aditya Sinha <adityasinhafb@gmail.com>
2022-08-29 19:22:27 -0700

    problem 507

---
+++ problem507.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.Scanner;
+import java.util.List;
+import java.util.*;
+import java.lang.*;
+
+class problem507{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+ 
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+ 
+            // the code is not running on an online judge
+ 
+            PrintStream ps
+                = new PrintStream(new File("output.txt"));
+            InputStream is
+                = new FileInputStream("input.txt");
+ 
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //For reading integer from input file
+        //int a=scan.nextInt();
+        //String temp1="leetcodeisacommunityforcoders";
+        //int temp2=4;
+        /*int [] input=new int[4];
+        for (int i = 0; i < input.length; i++) {
+            input[i]=scan.nextInt();
+        }*/
+
+        
+        //scan.nextLine();
+        //To take array as input
+        int num=7;
+        boolean sol=checkPerfectNumber(num);
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        System.out.println(sol);
+        scan.close();
+    }  
+
+    public static boolean checkPerfectNumber(int num) {
+        int n=num/2;
+        int sum=0;
+        while(n>0){
+            if(num%n==0){
+                sum+=n;
+            }
+            n--;
+        }
+        return sum==num?true:false;
+    }
+}  
