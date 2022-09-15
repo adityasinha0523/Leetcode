0425e838e940ccd9972c400b4c65b3d304dd6788
Aditya Sinha <adityasinhafb@gmail.com>
2022-09-15 10:51:31 -0700

    Array sorting 2D Array

--- problemArray.java
+++ problemArray.java
@@ -64,6 +64,21 @@ class problemArray{
          */
         Arrays.sort(A);
 
+        /*
+         * 3.Sorting 2D Array with second index in descending order
+         */
+
+        int[][] array2D=new int[3][3];
+        array2D[0][0]=1;
+        array2D[0][1]=2;
+        array2D[0][2]=3;
+        array2D[1][0]=4;
+        array2D[1][1]=5;
+        array2D[1][2]=6;
+        array2D[2][0]=7;
+        array2D[2][1]=8;
+        array2D[2][2]=9;
+        Arrays.sort(array2D, (a,b)->b[1]-a[1]);
         
         scan.close();
     }  
