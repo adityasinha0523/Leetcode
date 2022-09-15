a9a5957f81388a8494829499f68f21b40786daf8
Aditya Sinha <adityasinhafb@gmail.com>
2022-09-15 15:44:29 -0700

    problem 2351

---
+++ problem2351.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem2351{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+ 
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+ 
+            // the code is not running on an online judge
+ 
+            PrintStream ps
+                = new PrintStream(new File("output.txt"));
+            InputStream is
+                = new FileInputStream("input.txt");
+ 
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+
+        /*int[] nums=new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+        char sol=repeatedCharacter("abcbaacz");
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        System.out.println(sol);
+        scan.close();
+    }  
+
+    public static char repeatedCharacter(String s) {
+        char solution='a';
+        Set<Character> set=new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            if(set.contains(s.charAt(i))){
+                return s.charAt(i);
+            }else{
+                set.add(s.charAt(i));
+            }
+        }
+        return solution;
+    }
+}  
