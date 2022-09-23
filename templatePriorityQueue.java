35882e975964cf9b64ffadbdca6135ea9c74a998
Aditya Sinha <adityasinhafb@gmail.com>
2022-09-22 17:40:53 -0700

    problem 2357

---
+++ templatePriorityQueue.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+public class templatePriorityQueue {
+    public static void main(String[] args) {
+       // https://www.geeksforgeeks.org/max-heap-in-java/
+       /*
+        * 1.To add integer in decreasing order.
+        */
+        PriorityQueue<Integer> pQueue= new PriorityQueue<Integer>(Collections.reverseOrder());
+        /*
+         * 2.To add integer in ascending order.
+         */
+        PriorityQueue<Integer> pQueue1= new PriorityQueue<Integer>();
+    }    
+}
