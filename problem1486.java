cc113e36ef208be9690dfda2e60def521d712fec
Aditya Sinha <adityasinhafb@gmail.com>
2022-10-04 18:46:52 -0700

    problem 1486

---
+++ problem1486.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem1486{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+ 
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+ 
+            // the code is not running on an online judge
+ 
+            PrintStream ps
+                = new PrintStream(new File("output.txt"));
+            InputStream is
+                = new FileInputStream("input.txt");
+ 
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        /*int[] nums=new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+        int n=4;
+        int start=3;
+        int sol=xorOperation(n,start);
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        System.out.println(sol);
+        scan.close();
+    }  
+
+    public static int xorOperation(int n, int start) {
+        int solution=0;
+        int[] arr=new int[n];
+        arr[0]=start;
+        n=n-1;
+        int k=1;
+        while(n!=0){
+            arr[k]=start+2*k;
+            k++;
+            n--;
+        }
+        for (int i : arr) {
+            solution=solution^i;
+        }
+        return solution;
+    } 
+}  
