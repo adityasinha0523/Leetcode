c80bccd4cedfc898748052e58ff8ec2fd9a4bc8c
Aditya Sinha <adityasinhafb@gmail.com>
2022-11-04 18:36:18 -0700

    problem 2427

---
+++ problem1165.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem1165{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+ 
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+ 
+            // the code is not running on an online judge
+ 
+            PrintStream ps
+                = new PrintStream(new File("output.txt"));
+            InputStream is
+                = new FileInputStream("input.txt");
+ 
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        /*int[] nums=new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+        
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static int calculateTime(String keyboard, String word) {
+        Map<Character,Integer> map=new HashMap<>();
+        for (int i = 0; i < keyboard.length(); i++) {
+            map.put(keyboard.charAt(i), i);
+        }
+        int solution=0;
+        int lastValue=0;
+        for (int i = 0; i < word.length(); i++) {
+            char c=word.charAt(i);
+            int value=map.get(c);
+            solution+=Math.abs(lastValue-value);
+            lastValue=value;
+        }
+        return solution;
+    }
+}  
