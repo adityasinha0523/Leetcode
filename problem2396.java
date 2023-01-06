afbfbd5e12bf26e0ce377e50a508b56246973ad7
Aditya Sinha <adityasinhafb@gmail.com>
2023-01-06 12:26:17 -0700

    problem 2396

---
+++ problem2396.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem2396{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        /*int[] nums=new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+        System.out.println(Integer.toString(9,3));
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static boolean isStrictlyPalindromic(int n) {
+        boolean flag = true;
+        for(int i = 2; i < n-1; i++){
+            if(!check(Integer.toString(n,i))){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+    public static boolean check(String n){
+        int left = 0;
+        int right = n.length()-1;
+        while(left<right){
+            if(n.charAt(left)!=n.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}  
