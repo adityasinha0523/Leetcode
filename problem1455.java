774edd54c8b490048396e23f659fb54da9e6da13
Aditya Sinha <adityasinhafb@gmail.com>
2023-01-11 11:31:34 -0700

    problem 1455

---
+++ problem1455.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem1455{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        /*int[] nums=new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+        
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static int isPrefixOfWord(String sentence, String searchWord) {
+        String[] str=sentence.split(" ");
+        for (int i = 0; i < str.length; i++) {
+            boolean flag=false;
+            for (int j = 0; j < searchWord.length(); j++) {
+                if(str[i].charAt(j)!=searchWord.charAt(j)){
+                    flag=true;
+                    break;
+                }
+            }
+            if(flag==false){
+                return i+1;
+            }
+        }
+        return -1; 
+    }
+}  
