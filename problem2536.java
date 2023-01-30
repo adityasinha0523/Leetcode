d225cbf3bde2dcb71d18c4ef8eb2f4b9a608c675
Aditya Sinha <adityasinhafb@gmail.com>
2023-01-30 12:44:55 -0700

    problem2548

---
+++ problem2536.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem2536{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        /*int[] nums=new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+        
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static int[][] rangeAddQueries(int n, int[][] queries) {
+        int[][] sol=new int[n][n];
+        for (int i = 0; i < queries.length; i++) {
+            int firstRow=queries[i][0];
+            int firstCol=queries[i][1];
+            int secondRow=queries[i][2];
+            int secondCol=queries[i][3];
+            for (int j = firstRow; j <=secondRow; j++) {
+                for (int j2 = firstCol; j2 <=secondCol; j2++) {
+                    sol[j][j2]+=1;
+                }
+            }
+            
+        }
+        return sol;
+    }
+}  
---
+++ problem2548.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class template{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        /*int[] nums=new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+        
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static double maxPrice(int[][] items, int capacity) {
+        Arrays.sort(items, (a,b)->b[0]*a[1]-b[1]*a[0]);
+        double totalWeight=0;
+        double totalPrice=0;
+        int index=0;
+        while(totalWeight<=capacity && index<items.length){
+            int[] item=items[index];
+            double price=(double)item[0];
+            double weight=(double)item[1];
+            if(totalWeight+weight<=capacity){
+                totalWeight+=weight;
+                totalPrice+=price;
+            }else{
+                double diff=capacity-totalWeight;
+                totalWeight+=diff;
+                totalPrice+=diff/weight*price;
+            }
+            index++;
+        }
+        if(totalWeight<capacity){
+            return -1;
+        }
+        return totalPrice;
+    }
+}  
