ff8c31a653c4583535a02ee2bc0fbc1caa22459d
Aditya Sinha <adityasinhafb@gmail.com>
2023-02-02 12:44:53 -0700

    problem 2496

---
+++ problem2496.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem2496{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        /*int[] nums=new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+        
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static int maximumValue(String[] strs) {
+        int solution=0;
+        for (int i = 0; i < strs.length; i++) {
+            String s=strs[i];
+            boolean flag=false;
+            for (int j = 0; j < s.length(); j++) {
+                if(Character.isAlphabetic(s.charAt(j))){
+                    flag=true;
+                }
+            }
+            if(flag==true){
+                solution=Math.max(solution, s.length());
+            }else{
+                solution=Math.max(solution, Integer.parseInt(s));
+            }
+        }
+        return solution;
+    }
+}  
