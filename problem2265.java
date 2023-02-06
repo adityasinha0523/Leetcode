5482ffc6ae954c9a659d169997e59563e87e9ff0
Aditya Sinha <adityasinhafb@gmail.com>
2023-02-06 16:55:21 -0700

    problem 2265

---
+++ problem2265.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem2265{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        /*int[] nums=new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+        
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    int res=0;
+    public int averageOfSubtree(TreeNode root) {
+        helper(root);
+        return res;
+    }
+    public int[] helper(TreeNode root){
+        if(root==null){
+            return new int[]{0,0};
+        }
+        int[] left=helper(root.left);
+        int[] right=helper(root.right);
+        int currSum=left[0]+right[0]+root.val;
+        int currCount=left[1]+right[1]+1;
+        if(currSum/currCount==root.val){
+            res++;
+        }
+        
+        return new int[]{currSum,currCount};
+    }
+}  
