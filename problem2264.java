53b916c93dd82b05e73c254efb232b087f7f0136
Aditya Sinha <adityasinhafb@gmail.com>
2023-02-06 16:33:10 -0700

    problem 2264

---
+++ problem2264.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem2264{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        /*int[] nums=new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+        
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static String largestGoodInteger(String num) {
+        int solution=Integer.MIN_VALUE;
+        String sol="";
+        for (int i = 0; i < num.length()-2; i++) {
+            char first=num.charAt(i);
+            char second=num.charAt(i+1);
+            char third=num.charAt(i+2);
+            if(first==second && second==third && first==third){
+                String s=num.substring(i, i+3);
+                int val=Integer.parseInt(s);
+                if(val>solution){
+                    solution=val;
+                    sol=s;
+                }
+            }
+        }
+        return sol;
+    }
+}  
