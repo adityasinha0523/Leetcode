18ed9b1f321856d832947bbc6d773d2937fbe0f5
Aditya Sinha <adityasinhafb@gmail.com>
2023-02-06 20:10:35 -0700

    problem 2181

---
+++ problem2181.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem2181{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        /*int[] nums=new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+        
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static ListNode mergeNodes(ListNode head) {
+        ListNode curr, root = new ListNode();
+        curr = root;
+        int sum = 0;
+        while(head!=null){
+            if(head.val==0 && sum!=0){
+            curr.next = new ListNode(sum);
+            curr = curr.next;
+            sum = 0;
+        }
+        sum+=head.val;
+        head = head.next;
+    }
+    return root.next;
+    }
+}  
