5c2300562a25ff47aaf57a8ceecfa278d44e8e2d
Aditya Sinha <adityasinhafb@gmail.com>
2023-02-08 16:46:30 -0700

    problem 2144

---
+++ problem2144.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem2144{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        /*int[] nums=new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+        
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static int minimumCost(int[] cost) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < cost.length; i++) {
+            pq.add(cost[i]);
+        }
+        int sum=0;
+        int first=0;
+        int second=0;
+        while(!pq.isEmpty()){
+            if(!pq.isEmpty()){
+                first=pq.poll();
+            }if(!pq.isEmpty()){
+                second=pq.poll();
+            }if(!pq.isEmpty()){
+                pq.poll();
+            }
+            sum+=first+second;
+        }
+        return sum;
+    }
+}  
