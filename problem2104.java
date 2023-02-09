8628a4f9c9937cf6bbc5f0c95696c5c6a2b794ec
Aditya Sinha <adityasinhafb@gmail.com>
2023-02-08 19:10:18 -0700

    problem 2104

---
+++ problem2104.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem2104{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        /*int[] nums=new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+        
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static long subArrayRanges(int[] nums) {
+        long solution=0l;
+        for (int i = 0; i < nums.length; i++) {
+            int max=nums[i];
+            int min=nums[i];
+            for (int j = i+1; j < nums.length; j++) {
+                if(nums[j]>max){
+                    max=nums[j];
+                }else if(nums[j]<min){
+                    min=nums[j];
+                }
+                solution+=max-min;
+            }
+        }
+        return solution;
+    }
+}  
