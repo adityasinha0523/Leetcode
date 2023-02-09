54ad2b6f8f2f4e620f73abc89cb21695b1823316
Aditya Sinha <adityasinhafb@gmail.com>
2023-02-08 18:57:53 -0700

    problem 2063

---
+++ problem2063.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem2063{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        /*int[] nums=new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+        
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static long countVowels(String word) {
+        long solution=0l;
+        int counter=0;
+        for (int i = 0; i < word.length(); i++) {
+            if(word.charAt(i)=='a'||word.charAt(i)=='e'||word.charAt(i)=='i'||word.charAt(i)=='o'||word.charAt(i)=='u'){
+                counter++;
+                solution+=counter;
+            }
+        }
+        int first=0;
+        while(first<word.length()){
+            if(word.charAt(first)=='a'||word.charAt(first)=='e'||word.charAt(first)=='i'||word.charAt(first)=='o'||word.charAt(first)=='u'){
+                counter=counter-1;
+                solution+=counter;
+            }else{
+                solution+=counter;
+            }
+            first++;
+        }
+        return solution;
+    }
+}  
