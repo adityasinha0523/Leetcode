aabe6ef0c7b60a300567660130cf7ddb8af0e2a8
Aditya Sinha <adityasinhafb@gmail.com>
2023-02-26 02:11:40 -0700

    problem 2022

---
+++ SauravOA.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class SauravOA{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        /*int[] nums=new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+        String str="Hi! This is the article you have to format properly. Could you do that for me, please?";
+        String[] sol=wrapString(str, 16);
+        for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static String[] wrapString(String str, int width) {
+        List<StringBuilder> solution=new ArrayList<>();
+        
+        StringBuilder sb=new StringBuilder();
+
+    }
+}  
