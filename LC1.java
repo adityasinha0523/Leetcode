088665af5cbf73dfbc0482bf64abe6966eeaf7f2
Aditya Sinha <adityasinhafb@gmail.com>
2023-04-22 19:40:36 -0700

    Leetcode contest solution

---
+++ LC1.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class LC1{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        /*int[] nums=new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+        
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+
+    public static int sumOfMultiples(int n) {
+        int solution=0;
+        Set<Integer> set=new HashSet<>();
+        for (int i = 0; i <=n; i++) {
+            if(i%3==0){
+                if(!set.contains(i)){
+                    solution+=i;
+                    set.add(i);
+                }
+            }
+        }
+        for (int i = 0; i <=n; i++) {
+            if(i%5==0){
+                if(!set.contains(i)){
+                    solution+=i;
+                    set.add(i);
+                }
+            }
+        }
+        for (int i = 0; i <=n; i++) {
+            if(i%7==0){
+                if(!set.contains(i)){
+                    solution+=i;
+                    set.add(i);
+                }
+            }
+        }
+        return solution;
+    }
+}  
