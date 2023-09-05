538b6ea1608ac34f9c1e96a1ee4583bae1bc1bc9
Aditya Sinha <adityasinhafb@gmail.com>
2023-09-05 10:28:05 -0700

    problem 2833

---
+++ problem2833.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem2833{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        //1D Array
+        /*int[] nums=new int[3];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+
+        //Taking 2D Array as input
+        /*int[][] nums=new int[3][3];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                nums[i][j]=scan.nextInt();
+            }
+        }*/
+
+        //Taking String as input.
+        //String s=scan.nextLine();
+
+        
+        //Printing 1D Array.
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static boolean canBeEqual(String s1, String s2) {
+        for (int i = 0; i < s1.length()-2; i++) {
+            char c=s1.charAt(i);
+            char c2=s2.charAt(i);
+            char c3=s2.charAt(i+2);
+            if(c==c2 ||c==c3){
+                continue;
+            }else{
+                return false;
+            }
+        }
+        for (int i = s1.length()-1; i >=2; i--) {
+            char c=s1.charAt(i);
+            char c2=s2.charAt(i);
+            char c3=s2.charAt(i-2);
+            if(c==c2 ||c==c3){
+                continue;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}  
