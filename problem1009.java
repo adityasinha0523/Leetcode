4d8cfad61c24fdf95fe1d540ca581c988b786302
Aditya Sinha <adityasinhafb@gmail.com>
2023-09-20 09:15:11 -0700

    problem 1005

---
+++ problem1009.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem1009{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        //1D Array
+        /*int[] nums=new int[3];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+
+        //Taking 2D Array as input
+        /*int[][] nums=new int[3][3];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                nums[i][j]=scan.nextInt();
+            }
+        }*/
+
+        //Taking String as input.
+        //String s=scan.nextLine();
+
+
+        //Printing 1D Array.
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static int bitwiseComplement(int n) {
+        String val=Integer.toBinaryString(n);
+        StringBuilder sb=new StringBuilder();
+        for (int i = 0; i < val.length(); i++) {
+            if(val.charAt(i)=='0'){
+                sb.append('1');
+            }else{
+                sb.append('0');
+            }
+        }
+        int solution=0;
+        String sol=sb.toString();
+        int pow=1;
+        for (int i = sol.length()-1; i >=0; i--) {
+            int val1=Character.getNumericValue(sol.charAt(i));
+            solution+=val1==1?Math.pow(2,pow):0;
+            pow++;
+        }
+        return solution;
+    }
+}  
