55b6850f8d5f2cc10d6329dda04d20f9ab248d99
Aditya Sinha <adityasinhafb@gmail.com>
2023-09-20 09:25:57 -0700

    problem 2859

---
+++ problem2859.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem2859{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        //1D Array
+        /*int[] nums=new int[3];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+
+        //Taking 2D Array as input
+        /*int[][] nums=new int[3][3];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                nums[i][j]=scan.nextInt();
+            }
+        }*/
+
+        //Taking String as input.
+        //String s=scan.nextLine();
+        List<Integer> list=new ArrayList<>();
+        list.add(5);
+        list.add(10);
+        list.add(1);
+        list.add(5);
+        list.add(2);
+        int sol=sumIndicesWithKSetBits(list, 1);
+        //Printing 1D Array.
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        System.out.println(sol);
+        scan.close();
+    }  
+
+    public static int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+        int solution=0;
+        int[] bit=new int[nums.size()];
+        int counter=0;
+        for (int j = 0; j < bit.length; j++) {
+            bit[counter++]=Integer.bitCount(j);
+        }
+            
+        for (int i = 0; i < bit.length; i++) {
+            if(bit[i]==k){
+                solution+=nums.get(i);
+            }
+        }
+        return solution;
+    }
+}  
