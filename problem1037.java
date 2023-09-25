fe9e7a58f62cfe0812b8f321539df18c1a7c7f36
Aditya Sinha <adityasinhafb@gmail.com>
2023-09-25 12:19:32 -0700

    problem 1037

---
+++ problem1037.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem1037{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        //1D Array
+        /*int[] nums=new int[3];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+
+        //Taking 2D Array as input
+        /*int[][] nums=new int[3][3];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                nums[i][j]=scan.nextInt();
+            }
+        }*/
+
+        //Taking String as input.
+        //String s=scan.nextLine();
+
+
+        //Printing 1D Array.
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static boolean isBoomerang(int[][] points) {
+        int x1=points[0][0];
+        int y1=points[0][1];
+        int x2=points[1][0];
+        int y2=points[1][1];
+        int x3=points[2][0];
+        int y3=points[2][1];
+        return !((y2-y1)*(x3-x2)==(x2-x1)*(y3-y2));
+    }
+}  
