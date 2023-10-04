837f578598812bec80ee098688a7a9b20ba9039c
Aditya Sinha <adityasinhafb@gmail.com>
2023-10-04 14:26:22 -0700

    problem 1086

---
+++ problem1086.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem1086{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        //1D Array
+        /*int[] nums=new int[3];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+
+        //Taking 2D Array as input
+        /*int[][] nums=new int[3][3];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                nums[i][j]=scan.nextInt();
+            }
+        }*/
+
+        //Taking String as input.
+        //String s=scan.nextLine();
+
+
+        //Printing 1D Array.
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static int[][] highFive(int[][] items) {
+        int k=5;
+        TreeMap<Integer,Queue<Integer>> list=new TreeMap<>();
+        for (int[] is : items) {
+            int id=is[0];
+            int score=is[1];
+            if(!list.containsKey(id)){
+                list.put(id, new PriorityQueue<>(Collections.reverseOrder()));
+            }
+            list.get(id).add(score);
+        }
+
+        List<int[]> solution=new ArrayList<>();
+        for (int id : list.keySet()) {
+            int sum=0;
+            for (int i = 0; i < k; i++) {
+                sum+=list.get(id).poll();
+            }
+            solution.add(new int[]{id,sum/k});
+        }
+        int[][] solutionArr=new int[solution.size()][];
+        return solution.toArray(solutionArr);
+    }
+}  
