45a0346363a148e7e4eee3c3ac8a4ad660764b26
Aditya Sinha <adityasinhafb@gmail.com>
2023-10-05 09:34:42 -0700

    problem 1957

--- problem1957.java
+++ problem1957.java
@@ -7,26 +7,19 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.util.*;
 import java.lang.*;
 
-class problem1935{
+class problem1957{
     public static void main(String args[]) throws IOException{  
         if (System.getProperty("ONLINE_JUDGE") == null) {
             // Redirecting the I/O to external files
- 
             // as ONLINE_JUDGE constant is not defined which
             // means
- 
             // the code is not running on an online judge
- 
-            PrintStream ps
-                = new PrintStream(new File("output.txt"));
-            InputStream is
-                = new FileInputStream("input.txt");
- 
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
             System.setIn(is);
             System.setOut(ps);
         }
@@ -36,40 +29,56 @@ class problem1935{
         //scan.nextLine();
         //To take int array as input
         
-        /*int[] nums=new int[4];
+        //1D Array
+        /*int[] nums=new int[3];
         for (int i = 0; i < nums.length; i++) {
             nums[i]=scan.nextInt();
         }*/
-        String text="leet code";
-        String brokerString="e";
-        int sol=canBeTypedWords(text, brokerString);
+
+        //Taking 2D Array as input
+        /*int[][] nums=new int[3][3];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                nums[i][j]=scan.nextInt();
+            }
+        }*/
+
+        //Taking String as input.
+        //String s=scan.nextLine();
+
+
+        //Printing 1D Array.
         /*for(int i=0;i<sol.length;i++){
             System.out.println(sol[i]);
         }*/
-        System.out.println(sol);
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
         scan.close();
     }  
 
-    public static int canBeTypedWords(String text, String brokenLetters) {
-        Set<Character> set=new HashSet<>();
-        int solution=0;
-        for (int i = 0; i < brokenLetters.length(); i++) {
-            set.add(brokenLetters.charAt(i));
-        }
-        String[] strArr=text.split(" ");
-        for (int i = 0; i < strArr.length; i++) {
-            String key=strArr[i];
+    public static String makeFancyString(String s) {
+        while(true){
+            StringBuilder sb=new StringBuilder();
+            sb.append(s.charAt(0));
             boolean found=false;
-            for (int j = 0; j < key.length(); j++) {
-                if(set.contains(key.charAt(j))){
+            for (int i = 1; i < s.length()-1; i++) {
+                if(s.charAt(i)==s.charAt(i-1) && s.charAt(i)==s.charAt(i+1)){
+                    //sb.append(s.charAt(i));
+                    //sb.append(s.charAt(i));
                     found=true;
-                    break;
+                    continue;
+                }else{
+                    //sb.append(s.charAt(i-1));
+                    sb.append(s.charAt(i));
                 }
             }
+            s=sb.toString();
             if(found==false){
-                solution++;
+                break;
             }
         }
-        return solution;
+        return s;
     }
 }  
