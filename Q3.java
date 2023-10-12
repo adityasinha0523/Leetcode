f9edcfd77f80eb18fb6bd0164f22620b4333ac31
Aditya Sinha <adityasinhafb@gmail.com>
2023-10-11 20:26:54 -0700

    Property

--- Q3.java
+++ Q3.java
@@ -1,20 +1,9 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.util.*;
-import java.util.Map.Entry;
-
-import javax.swing.text.AbstractDocument.LeafElement;
-
-import java.lang.*;
 
 class Q3{
     public static void main(String args[]) throws IOException{  
