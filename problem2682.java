8a63ca11b2248408113f4bb121f024e0aaec6bd5
Aditya Sinha <adityasinhafb@gmail.com>
2023-11-02 13:13:36 -0700

    problem 2682

---
+++ problem2682.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+
+import CodeForceTemplate.InputReader;
+
+import java.lang.*;
+
+class problem2682{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        //1D Array
+        /*int[] nums=new int[3];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+
+        //Taking 2D Array as input
+        /*int[][] nums=new int[3][3];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                nums[i][j]=scan.nextInt();
+            }
+        }*/
+
+        //Taking String as input.
+        //String s=scan.nextLine();
+
+
+        //Printing 1D Array.
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static int[] circularGameLosers(int n, int k) {
+        boolean[] visited=new boolean[n];
+        int i=0;
+        int v=0;
+        while(visited[i%n]==false){
+            v++;
+            visited[i%n]=true;
+            i+=v*k;
+        }
+        int[] sol=new int[n-v];
+        int i1=0;
+        for (int j = 0; j <visited.length; j++) {
+            if(visited[j]==false){
+                sol[i1++]=j+1;
+            }
+        }
+        return sol;
+    }
+}  
