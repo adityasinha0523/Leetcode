5a272d7742e7581c6b041f1ade39cbb3463d86d7
Aditya Sinha <adityasinhafb@gmail.com>
2023-11-03 16:43:58 -0700

    problem 2437

---
+++ problem2437.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem2437{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        //1D Array
+        /*int[] nums=new int[3];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+
+        //Taking 2D Array as input
+        /*int[][] nums=new int[3][3];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                nums[i][j]=scan.nextInt();
+            }
+        }*/
+
+        //Taking String as input.
+        //String s=scan.nextLine();
+
+
+        //Printing 1D Array.
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public int countTime(String time) {
+        if(time.charAt(0)=='?' &&time.charAt(1)=='?'
+        && time.charAt(3)=='?' && time.charAt(4)=='?'){
+            return 24*60;
+        }
+        int res=1;
+        if(time.charAt(0)=='?' && time.charAt(1)=='?'){
+            res*=24;
+        }else if(time.charAt(0)=='?'){
+            res*=(time.charAt(1)-'0'>=4?2:3);
+        }else if(time.charAt(1)=='?'){
+            res*=(time.charAt(0)-'0'==2?4:10);
+        }
+
+        if(time.charAt(3)=='?' && time.charAt(4)=='?'){
+            res*=60;
+        }
+        else if(time.charAt(3)=='?'){
+            res*=6;
+        }else if(time.charAt(4)=='?'){
+            res*=10;
+        }
+        return res;
+    }
+}  
