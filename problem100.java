f8cd4dd7685e1349e9c871b78f200b82f4d97579
Aditya Sinha <adityasinhafb@gmail.com>
2023-11-08 08:09:44 -0700

    problem 100

--- problem100.java
+++ problem100.java
@@ -44,16 +44,26 @@ class problem100{
         scan.close();
     }  
 
+    
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null){
-            return true;
-        }if(p==null || q==null){
-            return false;
+        ans=true;
+        helper(p,q);
+        return ans;
+    }
+    
+    public void helper(TreeNode p,TreeNode q){
+        if(p!=null && q!=null){
+            if( p.val==q.val){
+                helper(p.left,q.left);
+                helper(p.right,q.right);
+            }else{
+                ans=false;
+            }
+        }else if(p==null &&q!=null){
+            ans=false;
+        }else if(q==null && p!=null){
+            ans=false;
         }
-        if(p.val!=q.val){
-                return false;
-        }
-        
-        return isSameTree(p.left, q.left) &&isSameTree(p.right, q.right);
+    }
     }
 }  
