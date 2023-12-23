fd615d7baa845032475cf454b1b527ce7c8de8db
Aditya Sinha <adityasinhafb@gmail.com>
2023-12-23 12:31:43 -0700

    problem 1436

--- problem2959.java
+++ problem2959.java
@@ -11,7 +11,7 @@ import java.io.PrintStream;
 import java.util.*;
 import java.lang.*;
 
-class problem1436{
+class problem2959{
     public static void main(String args[]) throws IOException{  
         if (System.getProperty("ONLINE_JUDGE") == null) {
             // Redirecting the I/O to external files
@@ -54,29 +54,14 @@ class problem1436{
         /*for (Integer integer : sol) {
             System.out.println(integer);
         }*/
+        /*for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                System.out.println( sol[i][j]);
+            }
+        }*/
         //System.out.println(sol);
         scan.close();
     }  
 
-    public static String destCity(List<List<String>> paths) {
-        Map<String,String> map=new HashMap<>();
-        List<String> list1=new ArrayList<>();
-        String solution="";
-        for (int i = 0; i < paths.size(); i++) {
-            List<String> list=paths.get(i);
-            String first=list.get(0);
-            String second=list.get(1);
-            map.put(first,second);
-            list1.add(first);
-            list1.add(second);
-            //list.add(second);
-        }
-        for (int i = 0; i < list1.size(); i++) {
-            String s=list1.get(i);
-            if(!map.containsKey(s)){
-                return s;
-            }
-        }
-        return solution;
-    }
+    
 }  
