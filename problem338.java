21daabb2da3f7efc1f0bd31b5b366fa862c72887
Aditya Sinha <adityasinhafb@gmail.com>
2024-01-18 18:59:42 -0700

    problem 338

--- problem338.java
+++ problem338.java
@@ -61,8 +61,19 @@ class problem338{
     public static int[] countBits(int n) {
         int[] ans=new int[n+1];
         for (int i = 0; i < n+1; i++) {
-            ans[i]=Integer.bitCount(i);
+            ans[i]=find(i+1);
         }
         return ans;
+    }
+
+    public static int find(int val){
+        int count=0;
+        while(val!=0){
+            if((val&1)==1){
+                count++;
+            }
+            val=val>>1;
+        }
+        return count;
     }
 }  
