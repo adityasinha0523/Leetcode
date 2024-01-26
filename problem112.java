271f0d8c644c94c3e6f2c0c4253eee8c7dd05c5f
Aditya Sinha <adityasinhafb@gmail.com>
2024-01-25 18:20:14 -0700

    problem 112

---
+++ problem112.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem112{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        //1D Array
+        /*int[] nums=new int[3];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+
+        //Taking 2D Array as input
+        /*int[][] nums=new int[3][3];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                nums[i][j]=scan.nextInt();
+            }
+        }*/
+
+        //Taking String as input.
+        //String s=scan.nextLine();
+
+        
+        //Printing 1D Array.
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        /*for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                System.out.println( sol[i][j]);
+            }
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    boolean found=false;
+    public static boolean hasPathSum(TreeNode root, int targetSum) {
+        helper(root,targetSum,0);
+        return found;
+    }
+    
+    public static void helper(TreeNode root,int targetSum,int currentSum){
+        if(root==null){
+            return;
+        }
+        currentSum+=root.val;
+        if(root.left==null && root.right==null && currentSum==targetSum){
+            found=true;
+        }
+        helper(root.left,targetSum,currentSum);
+        helper(root.right,targetSum,currentSum);
+    }
+}  
