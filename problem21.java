6cdf80c67ed62d2b4e7164bd510fb44c6ac65b10
Aditya Sinha <adityasinhafb@gmail.com>
2024-01-30 16:12:22 -0700

    problem 21

---
+++ problem21.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+
+class problem21{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        //1D Array
+        /*int[] nums=new int[3];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+
+        //Taking 2D Array as input
+        /*int[][] nums=new int[3][3];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                nums[i][j]=scan.nextInt();
+            }
+        }*/
+
+        //Taking String as input.
+        //String s=scan.nextLine();
+
+        
+        //Printing 1D Array.
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        /*for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                System.out.println( sol[i][j]);
+            }
+        }*/
+        //System.out.println(sol);
+        scan.close();
+    }  
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode sol=new ListNode();
+        ListNode temp=sol;
+        while(list1!=null && list2!=null){
+            if(list1.val<=list2.val){
+                sol.next=list1;
+                list1=list1.next;
+                sol=sol.next;
+            }else{
+                sol.next=list2;
+                sol=sol.next;
+                list2=list2.next;
+            }
+        }
+        while(list1!=null){
+            sol.next=list1;
+            sol=sol.next;
+            list1=list1.next;
+        }
+        while(list2!=null){
+            sol.next=list2;
+            sol=sol.next;
+            list2=list2.next;
+        }
+        return temp.next;
+    }
+}  
