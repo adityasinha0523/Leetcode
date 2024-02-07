e98a9ab8de719c94eb1d13ce9c8c2a97f93cc0a3
Aditya Sinha <adityasinhafb@gmail.com>
2024-02-06 18:05:03 -0700

    problem 3

--- problem3.java
+++ problem3.java
@@ -69,4 +69,20 @@ class problem3{
         }
         return sol;
     }
+
+    public int lengthOfLongestSubstring2(String s) {
+        if(s.length()==0){
+            return 0;
+        }
+        HashMap<Character,Integer> map=new HashMap<>();
+        int max=0;
+        for (int i = 0,j=0; i < s.length(); i++) {
+            if(map.containsKey(s.charAt(i))){
+                j=Math.max(j, map.get(s.charAt(i))+1);
+            }
+            map.put(s.charAt(i), i);
+            max=Math.max(max, i-j+1);
+        }
+        return max;
+    }
 }  
