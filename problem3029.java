41bb8e17908c07f294f23e8c318ebe9e427f8f22
Aditya Sinha <adityasinhafb@gmail.com>
2024-02-07 09:46:40 -0700

    problem 3029

---
+++ problem3029.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter; 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+
+//import com.apple.laf.AquaButtonCheckBoxUI.CheckBoxButtonBorder;
+
+import java.lang.*;
+
+class problem3029{
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
+        
+        //1D Array
+        /*int[] nums=new int[3];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }*/
+
+        //Taking 2D Array as input
+        /*int[][] nums=new int[3][3];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                nums[i][j]=scan.nextInt();
+            }
+        }*/
+
+        //Taking String as input.
+        //String s=scan.nextLine();
+
+        int sol=minimumTimeToInitialState("abacaba", 3);
+        //Printing 1D Array.
+        /*for(int i=0;i<sol.length;i++){
+            System.out.println(sol[i]);
+        }*/
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        /*for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                System.out.println( sol[i][j]);
+            }
+        }*/
+        System.out.println(sol);
+        scan.close();
+    }  
+
+    public static boolean check(String word,int i){
+        int j=0;
+        while(i<word.length()){
+            if(word.charAt(i)==word.charAt(j)){
+                i++;
+                j++;
+            }else{
+                break;
+            }
+        }
+        return i==word.length();
+    }
+    public static int minimumTimeToInitialState(String word, int k) {
+        int n=word.length();
+
+        int count=1;
+        int i=k;
+        while(i<n){
+            if(check(word,i)){
+                break;
+            }
+            count++;
+            i=i+k;
+        }
+        return count;
+    }
+}  
