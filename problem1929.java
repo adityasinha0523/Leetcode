190c8a64540a247e3e836bb58bf228644fa00fba
Aditya Sinha <adityasinhafb@gmail.com>
2026-05-30 10:32:18 +0530

    problem 1929 updated

--- problem1929.java
+++ problem1929.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -11,44 +12,39 @@ import java.io.PrintStream;
 import java.util.*;
 import java.lang.*;
 class problem1929{  
-    public static void main(String args[]){  
+    public static void main(String args[]) throws IOException{  
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            // Redirecting the I/O to external files
+            // as ONLINE_JUDGE constant is not defined which
+            // means
+            // the code is not running on an online judge
+            PrintStream ps= new PrintStream(new File("output.txt"));
+            InputStream is= new FileInputStream("input.txt");
+            System.setIn(is);
+            System.setOut(ps);
+        }
+        Scanner scan = new Scanner(System.in);
+        //For reading string from input file
+        //String myLine = scan.nextLine();
+        //scan.nextLine();
+        //To take int array as input
         
-        //System.out.println("Hello Java"); 
-        int[] temp=new int[]{1,2,1};
-        int [] sol=getConcatenation(temp);
+        int[] nums=new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scan.nextInt();
+        }
+        int[] sol=getConcatenation(nums);
         for(int i=0;i<sol.length;i++){
             System.out.println(sol[i]);
         }
-    }
-    
-    public static int[] getConcatenation2(int[] nums) {
-        int[] answer=new int[nums.length*2];
-        int k=0;
-        for (int i = 0; i < answer.length; i++) {
-            if(k<nums.length){
-                answer[i]=nums[k++];
-            }else{
-                k=0;
-                answer[i]=nums[k++];
-            }
-            
-        }
-        return answer;
+        /*for (Integer integer : sol) {
+            System.out.println(integer);
+        }*/
+        //System.out.println(sol);
+        scan.close();
     }
 
-    /*public static int[] getConcatenation(int[] nums) {
-        int[] sol=new int[nums.length*2];
-        int k=0;
-        for (int i = 0; i < nums.length; i++) {
-            sol[k++]=nums[i];
-        }
-        for (int i = 0; i < nums.length; i++) {
-            sol[k++]=nums[i];
-        }
-        return sol;
-    }*/
-
-    public int[] getConcatenation(int[] nums) {
+    public static int[] getConcatenation(int[] nums) {
         /*int[] sol=new int[nums.length*2];
         int k=0;
         int l=nums.length;
